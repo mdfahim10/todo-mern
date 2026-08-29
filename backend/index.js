881c207d4b58c9b7847ff72a57a5b60348a8616c
Md Fahim <217421678+mdfahim10@users.noreply.github.com>
2026-08-29 17:35:21 +0530

    delete one

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import e from "express";
 import cors from "cors"
 import { collectionName, connection } from "./dbconfig.js";
+import { ObjectId } from "mongodb";
 const app = e();
 
 app.use(cors());
@@ -30,6 +31,26 @@ app.get("/tasks", async (req, res) => {
     if (result) {
         res.send({
             message: "Task List fetched",
+            success: true,
+            result
+        })
+    } else {
+        res.send({
+            message: "Error!try again later",
+            success: false,
+        })
+    }
+})
+
+
+app.delete("/delete/:id", async (req, res) => {
+    const db = await connection();
+    const id=req.params.id
+    const collection = await db.collection(collectionName);
+    const result = await collection.deleteOne({_id:new ObjectId(id)});
+    if (result) {
+        res.send({
+            message: "Task Deleted",
             success: true,
             result
         })
--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,5 +1,5 @@
 import "../styles/List.css"
-import { useEffect, useState } from "react"
+import { Fragment, useEffect, useState } from "react"
 
 export default function List() {
 
@@ -15,8 +15,20 @@ export default function List() {
         if (list.success) {
             setTaskData(list.result)
         }
-
     }
+
+    const deleteTask = async (id) => {
+        let item = await fetch("http://localhost:3200/delete/"+id,{
+            method:"DELETE"
+        });
+        item = await item.json();
+        if (item.success) {
+            getListData()
+            
+        }
+    }
+
+
     return (
         <div>
             <h1>To Do List</h1>
@@ -29,12 +41,12 @@ export default function List() {
                 {
 
                     taskData && taskData.map((item, index) => (
-                        <>
+                        <Fragment key={item._id}>
                             <li className="list-item">{index+1}</li>
                             <li className="list-item">{item.title}</li>
                             <li className="list-item">{item.description}</li>
-                            <li className="list-item"> <button>Delete</button></li>
-                        </>
+                            <li className="list-item"> <button onClick={()=>deleteTask(item._id)}>Delete</button></li>
+                        </Fragment>
                     ))
 
                 }
